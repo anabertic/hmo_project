5f516dffc2919686c92f58907f743d2f3c12700e
rsbuca <rsb.git@coder.hr>
2019-01-12 18:50:54 +0100

    Added attributes, contructor, getters and setters to Student class.

--- Group Swap/src/fer/hmo/models/Student.java
+++ Group Swap/src/fer/hmo/models/Student.java
@@ -1,5 +1,66 @@
 package fer.hmo.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
+	private int studentId;
+	private List<Integer> activityIds;
+	private List<Integer> swapWeights;
+	private List<Group> groups;
+	private List<Group> newGroups;
+	
+	public Student(int studentId) {
+		this.studentId = studentId;
+		
+		this.activityIds = new ArrayList<>();
+		this.swapWeights = new ArrayList<>();
+		this.groups = new ArrayList<>();
+		this.newGroups = new ArrayList<>();
+	}
+	
+	// ----- GETTERS AND SETTERS -----
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public List<Integer> getActivityIds() {
+		return activityIds;
+	}
+
+	public void setActivityIds(List<Integer> activityIds) {
+		this.activityIds = activityIds;
+	}
+
+	public List<Integer> getSwapWeights() {
+		return swapWeights;
+	}
+
+	public void setSwapWeights(List<Integer> swapWeights) {
+		this.swapWeights = swapWeights;
+	}
+
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
+
+	public List<Group> getNewGroups() {
+		return newGroups;
+	}
+
+	public void setNewGroups(List<Group> newGroups) {
+		this.newGroups = newGroups;
+	}
+	
+	// ----- END GETTERS AND SETTERS -----
 }
